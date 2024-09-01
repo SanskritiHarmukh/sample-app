f8aa4c43499fcffbce4e5dd5bbf5346043710725
SanskritiHarmukh <sanskritiharmukh1908@gmail.com>
2024-09-01 23:59:52 +0530

    updates tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
-# tests/test_app.py
-from app.main import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+
+from main import app 
 
 def test_home():
     with app.test_client() as client:
