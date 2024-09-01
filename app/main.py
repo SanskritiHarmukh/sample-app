91bee6f6ce04cdd59fdf5f660ac06f9d45b4801f
SanskritiHarmukh <sanskritiharmukh1908@gmail.com>
2024-09-01 23:20:47 +0530

    changed print message

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     out = (
-        f'Greetings Hackbyte Hackers'
+        f'Greetings fellas'
     )
     return out
  
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,4 @@ def test_home():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
-        assert b'Greetings Hackbyte Hackers' in response.data
+        assert b'Greetings fellas' in response.data
