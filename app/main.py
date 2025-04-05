9710b5d487885941b27ae59fbccadbee77da47e4
SanskritiHarmukh <sanskritiharmukh1908@gmail.com>
2025-04-05 11:39:30 +0530

    update: greetings message

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     out = (
-        f'Greetings!\n'
+        f'Greetings all!\n'
     )
     return out
  
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,4 @@ def test_home():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
-        assert b'Greetings!' in response.data
+        assert b'Greetings all!' in response.data
